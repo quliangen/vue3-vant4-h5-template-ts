029ab684d1e0eda638dde6c7ecf8d477a9180577
langen qu <quliangen@ktjr.com>
2020-09-18 23:29:01 +0800

    chore:添加prettier配置并格式化代码

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  printWidth: 120, // 120列换行
+  singleQuote: true, //用单引号
+  semi: true, //句末加分号
+  arrowParens: 'avoid', // 箭头函数一个参数无需加()
+  trailingComma: 'all', // {} []句末逗号
+};
