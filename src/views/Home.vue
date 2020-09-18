ddf4712143b84a755f69e514d727ac7f60740837
langen qu <quliangen@ktjr.com>
2020-09-18 23:36:33 +0800

    chore:运行lint添加pug并修改Home.vue为pug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+<template lang="pug">
+.home
+  img(alt="Vue logo" src="../assets/logo.png")
+  HelloWorld(msg="Welcome to Your Vue.js + TypeScript App")
 </template>
 
 <script lang="ts">
